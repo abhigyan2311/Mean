fbc447eb7549bc27a348381433a781798d608c79
Abhigyan Singh <abhigyan2311@gmail.com>
2015-06-15 16:12:07 +0530

    Initialize

---
+++ ng/posts.ctrl.js
@@ -0,0 +1,18 @@
+angular.module('app')
+    .controller('PostsCtrl', function ($scope, PostSvc) {
+        $scope.addPost = function () {
+            if ($scope.postBody) {
+                PostSvc.create({
+                    username: 'abhigyan',
+                    body: $scope.postBody
+                }).success(function (post) {
+                    $scope.posts.unshift(post)
+                    $scope.postBody = null
+                })
+            }
+        }
+
+        PostSvc.fetch().success(function (posts) {
+            $scope.posts = posts
+        })
+    })
